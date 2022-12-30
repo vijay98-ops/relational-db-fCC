0b1609d925e3739dc8e53c647ab2f27e42643f85
vijay98-ops <82332896+vijay98-ops@users.noreply.github.com>
2022-12-30 14:32:59 +0545

    Create insert_data.sh
    Till now it can only insert teams into teams table.

---
+++ worldcup_db/insert_data.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+cat games.csv | while IFS=',' read YR ROUND WINR OPPON WG OG
+do
+  if [[ $WINR != 'winner' ]]
+  then
+    #get team_id
+    win_id=$($PSQL "SELECT team_id FROM teams WHERE name='$WINR'")
+
+    #if team_id not found, insert the team    
+    if [[ -z $win_id ]]
+    then
+      #insert team into teams
+      INSERT_TEAM=$($PSQL "INSERT INTO teams(name) VALUES('$WINR')")
+    fi
+  fi
+
+  if [[ $OPPON != 'opponent' ]]
+  then
+    #get team_id
+    opp_id=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPON'")
+    if [[ -z $opp_id ]]
+    then
+      INSERT_TEAM=$($PSQL "INSERT INTO teams(name) VALUES('$OPPON')")
+    fi
+  fi
+done
