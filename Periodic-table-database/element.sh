b62589ec225f02d97fe5032dd424b21a7c390183
vijay98-ops <82332896+vijay98-ops@users.noreply.github.com>
2023-03-15 19:27:16 +0545

    Update element.sh

--- Periodic-table-database/element.sh
+++ Periodic-table-database/element.sh
@@ -2,33 +2,30 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-
-#If no input is provided: 
+#if no input is provided
 if [[ -z $1 ]]
 then
   echo Please provide an element as an argument.
   exit
 fi
 
-#check if the argument provided is an atomic number: 
+#check if the arg provided is a number
 if [[ $1 =~ ^[1-9]+$ ]]
 then
-  RESULT=$($PSQL " SELECT atomic_number, types.type, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name FROM properties JOIN elements USING (atomic_number) JOIN types USING (type_id) WHERE atomic_number = $1 ")
-
-#If not a number, then it must be a string:
+  RESULT=$($PSQL "SELECT atomic_number,types.type,atomic_mass,melting_point_celsius,boiling_point_celsius,symbol,name FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE atomic_number=$1")
+#if not a number, could be a string
 else
-  RESULT=$($PSQL " SELECT atomic_number, types.type, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name FROM properties JOIN elements USING (atomic_number) JOIN types USING (type_id) WHERE name = '$1' OR symbol = '$1' ")
-
+  RESULT=$($PSQL "SELECT atomic_number,types.type,atomic_mass,melting_point_celsius,boiling_point_celsius,symbol,name FROM properties JOIN elements USING(atomic_number) JOIN types USING(type_id) WHERE name='$1' OR symbol='$1'")
 fi
 
-#If element not found:
+#if element not found
 if [[ -z $RESULT ]]
 then
   echo -e "I could not find that element in the database."
   exit
 fi
 
-echo $RESULT | while IFS=" |" read ATOMIC_NUMBER TYPE MASS MELTING_POINT BOILING_POINT SYMBOL NAME
+echo $RESULT | while IFS=" |" read ATNO TYPE MASS MLT BLT SYMB NAME
 do
-  echo -e "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  echo -e "The element with atomic number $ATNO is $NAME ($SYMB). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MLT celsius and a boiling point of $BLT celsius."
 done
